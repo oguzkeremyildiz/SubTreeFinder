1dbefcf7551ec26bdce8f0cabfe5263265ece2ef
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2024-06-29 02:42:43 +0300

    Initial Commit.

--- src/main/java/org/example/QnoConstituencyRulesGenerator.java
+++ src/main/java/org/example/QnoConstituencyRulesGenerator.java
@@ -9,56 +9,37 @@ import java.util.*;
 
 public class QnoConstituencyRulesGenerator {
 
-    private static ArrayList<ParseNode> dfs(HashMap<ParseNode, ArrayList<ParseNode>> rangeMap, ParseNode current) {
-        ArrayList<ParseNode> result = new ArrayList<>();
+    private static Pair<Integer, Integer> dfs(HashMap<Integer, ArrayList<Pair<Integer, String>>> rangeMap, ParseNode current, int index, HashMap<String, String> tagMap) {
         if (current.isLeaf()) {
-            result.add(current);
-            return result;
+            return new Pair<>(index + 1, index + 1);
         }
+        int lastIndex = index;
         for (int i = 0; i < current.numberOfChildren(); i++) {
             ParseNode child = current.getChild(i);
-            result.addAll(dfs(rangeMap, child));
+            Pair<Integer, Integer> p = dfs(rangeMap, child, lastIndex, tagMap);
+            lastIndex = p.getValue();
         }
-        rangeMap.put(current, (ArrayList<ParseNode>) result.clone());
-        return result;
-    }
-
-    private static HashMap<ParseNode, ArrayList<ParseNode>> calculateNodeRange(ParseNode root) {
-        HashMap<ParseNode, ArrayList<ParseNode>> rangeMap = new HashMap<>();
-        dfs(rangeMap, root);
-        return rangeMap;
-    }
-
-    private static HashMap<ParseNode, Integer> calculateNoMap(ArrayList<ParseNode> leaves) {
-        HashMap<ParseNode, Integer> map = new HashMap<>();
-        for (int i = 0; i < leaves.size(); i++) {
-            map.put(leaves.get(i), i);
-        }
-        return map;
-    }
-
-    private static void calculatePairMap(HashMap<Integer, ArrayList<Pair<Integer, String>>> pairMap, HashSet<String> tags, HashMap<ParseNode, ArrayList<ParseNode>> rangeMap, HashMap<ParseNode, Integer> noMap, ParseNode current) {
-        if (!current.isLeaf()) {
-            String currentData = current.getData().getName();
-            String childData = current.getChild(0).getData().getName();
-            ArrayList<ParseNode> range = rangeMap.get(current);
-            // do, did, does will be added.
-            if (childData.equals("how") || childData.equals("much") || childData.equals("many") || childData.equals("what") || childData.equals("when") || childData.equals("where") || childData.equals("which") || childData.equals("is") || childData.equals("are") || childData.equals("was") || childData.equals("were")) {
-                if (!pairMap.containsKey(noMap.get(range.get(0)))) {
-                    pairMap.put(noMap.get(range.get(0)), new ArrayList<>());
-                }
-                pairMap.get(noMap.get(range.get(0))).add(new Pair<>(noMap.get(range.get(range.size() - 1)), childData));
-            } else if (tags.contains(currentData)) {
-                if (!pairMap.containsKey(noMap.get(range.get(0)))) {
-                    pairMap.put(noMap.get(range.get(0)), new ArrayList<>());
-                }
-                pairMap.get(noMap.get(range.get(0))).add(new Pair<>(noMap.get(range.get(range.size() - 1)), currentData));
+        String currentData = current.getData().getName();
+        String childData = current.getChild(0).getData().getName();
+        // do, did, does will be added.
+        if (childData.equals("how") || childData.equals("much") || childData.equals("many") || childData.equals("what") || childData.equals("when") || childData.equals("where") || childData.equals("which") || childData.equals("is") || childData.equals("are") || childData.equals("was") || childData.equals("were")) {
+            if (!rangeMap.containsKey(index)) {
+                rangeMap.put(index, new ArrayList<>());
             }
-            for (int i = 0; i < current.numberOfChildren(); i++) {
-                ParseNode child = current.getChild(i);
-                calculatePairMap(pairMap, tags, rangeMap, noMap, child);
+            rangeMap.get(index).add(new Pair<>(lastIndex - 1, childData));
+        } else if (tagMap.containsKey(currentData)) {
+            if (!rangeMap.containsKey(index)) {
+                rangeMap.put(index, new ArrayList<>());
             }
+            rangeMap.get(index).add(new Pair<>(lastIndex - 1, currentData));
         }
+        return new Pair<>(index, lastIndex);
+    }
+
+    private static Pair<HashMap<Integer, ArrayList<Pair<Integer, String>>>, Integer> calculateNodeRange(ParseNode root, HashMap<String, String> tagMap) {
+        HashMap<Integer, ArrayList<Pair<Integer, String>>> rangeMap = new HashMap<>();
+        Pair<Integer, Integer> p = dfs(rangeMap, root, 0, tagMap);
+        return new Pair<>(rangeMap, p.getValue() - 1);
     }
 
     private static String removePunctuations(ArrayList<String> list) {
@@ -90,17 +71,13 @@ public class QnoConstituencyRulesGenerator {
         TreeBank bank = new TreeBank(new File("Trees"));
         Scanner source = new Scanner(new File("automatic-qno-search-list.txt"));
         CounterHashMap<String> possibilities = new CounterHashMap<>();
-        HashSet<String> tags = new HashSet<>();
         HashMap<String, String> tagMap = new HashMap<>();
-        HashSet<String> tagSet = new HashSet<>();
         while (source.hasNextLine()) {
             String line = source.nextLine();
             String[] tokens = line.split(" ");
             for (int i = 1; i < tokens.length; i++) {
                 String[] parts = tokens[i].split("/");
-                Collections.addAll(tags, parts);
-                if (!tagSet.contains(tokens[i])) {
-                    tagSet.add(tokens[i]);
+                if (!tagMap.containsKey(parts[0])) {
                     for (String part : parts) {
                         tagMap.put(part, tokens[i]);
                     }
@@ -108,18 +85,11 @@ public class QnoConstituencyRulesGenerator {
             }
         }
         source.close();
-        HashMap<ParseNode, ArrayList<ParseNode>> rangeMap;
-        HashMap<ParseNode, Integer> noMap;
-        HashMap<Integer, ArrayList<Pair<Integer, String>>> pairMap = new HashMap<>();
         for (int i = 0; i < bank.size(); i++) {
             ParseTree parseTree = bank.get(i);
             ParseNode root = parseTree.getRoot();
-            rangeMap = calculateNodeRange(root);
-            noMap = calculateNoMap(rangeMap.get(root));
-            int treeSize = noMap.get(rangeMap.get(root).get(rangeMap.get(root).size() - 1));
-            calculatePairMap(pairMap, tags, rangeMap, noMap, root);
-            backtrack(pairMap, tagMap, possibilities, new ArrayList<>(), 0, treeSize);
-            pairMap.clear();
+            Pair<HashMap<Integer, ArrayList<Pair<Integer, String>>>, Integer> pair = calculateNodeRange(root, tagMap);
+            backtrack(pair.getKey(), tagMap, possibilities, new ArrayList<>(), 0, pair.getValue());
         }
         List<Map.Entry<String, Integer>> topNList = possibilities.topN(1000);
         for (Map.Entry<String, Integer> entry : topNList) {
